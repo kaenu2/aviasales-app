9d35244addee416d8dbe94fb61315dacd8707eb9
Andrey Kurnosov <beezdar123455@gmail.com>
2023-11-03 03:01:25 +0300

    fix store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
-import { applyMiddleware, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from './reducers';
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = configureStore({ reducer: rootReducer, middleware: [thunk], devTools: {} });
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 
 import { sortReducer } from './sortReducer';
 import { transfersReducer } from './transfersReducer';
